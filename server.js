e2b7d3bb74c9693c4e34994e7c68101a77710cdd
buhari.maiwada <maewada@live.com>
2021-08-31 22:38:06 -0400

    added config folder and modified server.js

--- server.js
+++ server.js
@@ -10,17 +10,16 @@ const bodyParser = require('body-parser');
 const app = express(); //initialize our app
 
 //BodyParser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
 
 //DB Config
 const db = require('./config/keys').mongoURI
 
 //Connect to MongoDB
-mongoose.connect(db, { userNewUrlParser: true })
+mongoose.connect(db, { userNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('MongoDB successfully connected'))
-    .catch(err => console.log('MongoDB error'));
+    .catch((err) => console.error('Not Connected') );
 
 const port = process.env.PORT || 5000; //process.env.port is Heroku's port if you choose to deploy the app.
 
-app.listen(port, () => console.log('Server up and running on port ${port}!'));
+app.listen(port, () => console.log(`Server up and running on port ${port}!`));
